faf59469b703e635bc13fa9e6d36ffbbb9e8451b
Sean M. Kelly <dacodestah@yahoo.com>
2015-09-15 23:17:31 -0700

    Foundational stuffs and some notes

--- src/net/javagoodies/MicroService/MicroService.java
+++ src/net/javagoodies/MicroService/MicroService.java
@@ -1,12 +1,23 @@
 package net.javagoodies.MicroService;
 
 /**
+ * MicroService base class
  *
+ * We are going to use dagger for dependency injection...
+ * ref: http://square.github.io/dagger/
+ *
+ * Read more about dependency management options:
+ * ref: http://zeroturnaround.com/rebellabs/java-build-tools-how-dependency-management-works-with-maven-gradle-and-ant-ivy/
  */
 public class MicroService {
 
 	/**
+	 * @todo dependency inject:
 	 *
+	 * * Config class
+	 * * Logger class
+	 * * Monitor class
+	 * * Reporter class
 	 */
 	public MicroService() {
 	}
